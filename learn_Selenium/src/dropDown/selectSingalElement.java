44937455d65d25f5116a3ef92c31f4e1fd80811a
HP <HP@DESKTOP-EGOL3EF>
2023-12-31 15:54:10 +0530

    first commit

---
+++ learn_Selenium/src/dropDown/selectSingalElement.java
@@ -0,0 +1,10 @@
+package dropDown;
+
+public class selectSingalElement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ learn_Selenium/src/screenShort/entearPage.java
@@ -0,0 +1,10 @@
+package screenShort;
+
+public class entearPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
